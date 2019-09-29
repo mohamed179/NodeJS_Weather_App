0afa9a7880f437ad1092d370d1f9f08b44a994a5
mohamed179 <mohamed77zaki@gmail.com>
2019-09-29 02:11:17 +0200

    Section 6 Step 5

--- app.js
+++ app.js
@@ -1,8 +1,10 @@
 const request = require('request');
 
-const url = 'https://api.darksky.net/forecast/069a64a72d369e5f25b3cd5151a4042f/37.8267,-122.4233';
+// const url = 'https://api.darksky.net/forecast/069a64a72d369e5f25b3cd5151a4042f/37.8267,-122.4233?exclude=minutely,hourly,daily';
+const url = 'https://api.darksky.net/forecast/069a64a72d369e5f25b3cd5151a4042f/37.8267,-122.4233?lang=zh';
 
-request({ url: url }, (error, response) => {
-  const weatherData = JSON.parse(response.body);
-  console.log(weatherData.currently);
+request({ url: url, json: true }, (error, response) => {
+  const temperature = response.body.currently.temperature;
+  const precipProbability = response.body.currently.precipProbability;
+  console.log(`${response.body.daily.data[0].summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`);
 });
